c4f809dc5ba2071ec1def0481a6648b6da7be821
OlgaOvcharenko <44943723+OlgaOvcharenko@users.noreply.github.com>
2025-10-17 06:30:18 +0200

    Improve sem_extract_features and its tests (#42)
    Thanks, Olga!

--- tests/test_sem_extract_features_image.py
+++ tests/test_sem_extract_features_image.py
@@ -1,37 +1,95 @@
 import pandas as pd
 import pytest  # pylint: disable=import-error
 import skrub
-from sklearn.model_selection import cross_validate
 
 import sempipes  # pylint: disable=unused-import
 
 
-@pytest.mark.skip(reason="Currently disabled because its too costly")
-def test_sem_extract_features_image():
-    styles = pd.read_csv("tests/data/fashion-dataset/styles.csv", on_bad_lines="skip")
+def test_sem_extract_features_image_code():
+    styles_df = pd.read_csv("tests/data/fashion-dataset/styles.csv", on_bad_lines="skip")
+    styles = skrub.var("styles", styles_df)
 
     X_columns = ["gender", "season", "year", "productDisplayName", "baseColour", "usage"]
     y_column = "masterCategory"
 
     # Train over texts
-    model = skrub.tabular_pipeline("classifier")
-    results = cross_validate(model, styles[X_columns], styles[y_column])
-    print(f"Tabular predictor performance w/o extracted features: {results["test_score"]}")
+    X = styles[X_columns].skb.mark_as_X()
+    y = styles[y_column].skb.mark_as_y()
+
+    pipeline = skrub.tabular_pipeline("classifier")
+    task = X.skb.apply(pipeline, y=y)
+    split = task.skb.train_test_split(random_state=0)
+    learner = task.skb.make_learner()
+    learner.fit(split["train"])
+    score_before = learner.score(split["test"])
+
+    print(f"Tabular predictor performance w/o extracted features: {score_before}")
 
     # Extract pictures
-    styles_ref = skrub.var("styles", styles)
-    styles_ref = styles_ref.sem_extract_features(
+    styles_full = skrub.var("styles_full", styles_df)
+    X_with_features = styles_full[X_columns + ["full_path"]]
+    y_new = styles_full[y_column].skb.mark_as_y()
+
+    X_with_features = X_with_features.sem_extract_features(
         nl_prompt="Extract up to three features from the product image and/or the product display name that can be used for the product master category prediction. The features should be very fine-grained and helpful.",
         input_columns=["productDisplayName", "full_path"],
-    ).skb.eval()
-
-    X_columns_to_remove = ["subCategory", "id", "full_path", "masterCategory", "articleType"]
-    X_columns_with_new_features = styles.columns[~styles.columns.isin(X_columns_to_remove)]
-
-    model_with_new_features = skrub.tabular_pipeline("classifier")
-    results_with_new_features = cross_validate(
-        model_with_new_features, styles_ref[X_columns_with_new_features], styles[y_column]
+        generate_via_code=True,
     )
-    print(f"Tabular predictor performance with extracted features: {results_with_new_features["test_score"]}")
 
-    assert (results_with_new_features["test_score"] >= results["test_score"]).all()
+    X_with_features = X_with_features.skb.drop(["full_path"])
+    X_with_features = X_with_features.skb.mark_as_X()
+
+    pipeline_with_features = skrub.tabular_pipeline("classification")
+    task_with_features = X_with_features.skb.apply(pipeline_with_features, y=y_new)
+    split_with_features = task_with_features.skb.train_test_split(random_state=0)
+    learner_with_features = task_with_features.skb.make_learner()
+    learner_with_features.fit(split_with_features["train"])
+    score_with_features = learner_with_features.score(split_with_features["test"])
+    print(f"Tabular predictor performance with extracted features: {score_with_features}")
+
+    assert score_before <= score_with_features
+
+
+@pytest.mark.skip(reason="Currently disabled because its too costly")
+def test_sem_extract_features_image():
+    styles_df = pd.read_csv("tests/data/fashion-dataset/styles.csv", on_bad_lines="skip")
+    styles = skrub.var("styles", styles_df)
+
+    X_columns = ["gender", "season", "year", "productDisplayName", "baseColour", "usage"]
+    y_column = "masterCategory"
+
+    # Train over texts
+    X = styles[X_columns].skb.mark_as_X()
+    y = styles[y_column].skb.mark_as_y()
+
+    pipeline = skrub.tabular_pipeline("classifier")
+    task = X.skb.apply(pipeline, y=y)
+    split = task.skb.train_test_split(random_state=0)
+    learner = task.skb.make_learner()
+    learner.fit(split["train"])
+    score_before = learner.score(split["test"])
+
+    print(f"Tabular predictor performance w/o extracted features: {score_before}")
+
+    # Extract pictures
+    styles_full = skrub.var("styles_full", styles_df)
+    X_with_features = styles_full[X_columns + ["full_path"]]
+    y = styles_full[y_column].skb.mark_as_y()
+
+    X_with_features = X_with_features.sem_extract_features(
+        nl_prompt="Extract up to three features from the product image and/or the product display name that can be used for the product master category prediction. The features should be very fine-grained and helpful.",
+        input_columns=["productDisplayName", "full_path"],
+    )
+
+    X_with_features = X_with_features.skb.drop(["full_path"])
+    X_with_features = X_with_features.skb.mark_as_X()
+
+    pipeline_with_features = skrub.tabular_pipeline("classification")
+    task_with_features = X_with_features.skb.apply(pipeline_with_features, y=y)
+    split_with_features = task_with_features.skb.train_test_split(random_state=0)
+    learner_with_features = task_with_features.skb.make_learner()
+    learner_with_features.fit(split_with_features["train"])
+    score_with_features = learner_with_features.score(split_with_features["test"])
+    print(f"Tabular predictor performance with extracted features: {score_with_features}")
+
+    assert score_before <= score_with_features
