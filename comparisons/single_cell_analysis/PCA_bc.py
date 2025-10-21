3f9808e5db932e501444c8a90540f672033ed434
OlgaOvcharenko <44943723+OlgaOvcharenko@users.noreply.github.com>
2025-10-21 05:34:29 +0200

    Comparison for single cell integration (#41)

---
+++ comparisons/single_cell_analysis/PCA_bc.py
@@ -0,0 +1,63 @@
+# pylint: skip-file
+import numpy as np
+import scanpy as sc
+from sklearn.decomposition import PCA
+import scanpy as sc
+from scib_metrics.benchmark import BatchCorrection, Benchmarker, BioConservation
+
+
+
+def get_representation(adata, batch_key, cell_type):
+    # Apply PCA
+    pca = PCA()
+    X_pca = pca.fit_transform(adata.X)
+
+    adata.obsm["pca"] = X_pca
+
+    return adata
+
+
+def evaluate_model(adata, batch_key, cell_type_label):
+    _BIO_METRICS = BioConservation(
+        isolated_labels=True,
+        nmi_ari_cluster_labels_leiden=True,
+        nmi_ari_cluster_labels_kmeans=False,
+        silhouette_label=True,
+        clisi_knn=True,
+    )
+    _BATCH_METRICS = BatchCorrection(
+        graph_connectivity=True, 
+        kbet_per_label=True, 
+        ilisi_knn=True, 
+        pcr_comparison=True, 
+        bras=True
+    )
+    
+    names_obsm = ["pca"]
+    print(names_obsm)
+    bm = Benchmarker(
+        adata,
+        batch_key=batch_key,
+        label_key=cell_type_label,
+        embedding_obsm_keys=names_obsm,
+        bio_conservation_metrics=_BIO_METRICS,
+        batch_correction_metrics=_BATCH_METRICS,
+        n_jobs=4,
+    )
+    bm.benchmark()
+    a = bm.get_results(False, True)
+    results = a.round(decimals=4)
+    return results
+
+
+adata_path = "comparisons/single_cell_analysis/data/ImmuneAtlas_raw.h5ad"
+adata = sc.read(adata_path)
+batch_key = "batchlb"
+cell_type = "cell_type"
+
+adata = get_representation(adata, batch_key=batch_key, cell_type=cell_type)
+results = evaluate_model(adata=adata, batch_key=batch_key, cell_type_label=cell_type)
+
+print(results)
+
+results.to_csv("comparisons/single_cell_analysis/results_unscaled_pca.csv")
---
+++ comparisons/single_cell_analysis/data/README.md
@@ -0,0 +1,7 @@
+Immune Cell Atlas: This dataset contains 329,762 cells and includes 36,398 genes across twelve batches and three different
+sequencing technologies (10x 5’ v1, 10x 5’ v2, 10x 3’ v3), collected by Conde et al.(2022). The cells originate from 16
+different tissues. The annotations include 35 fine-grain cell types. 
+
+We use 10x 5’ v2 technology as hold-out (query) data and 10x 5’ v1, 10x 3’ v3 as train (reference) data.
+
+Download data: [link](https://datasets.cellxgene.cziscience.com/08f58b32-a01b-4300-8ebc-2b93c18f26f7.h5ad) and put into `comparisons/single_cell_analysis/data/ImmuneAtlas_raw.h5ad`.
